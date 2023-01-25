b296d595bdb0db40efa4f25d1410bd6eeb9479d8
DCasa23 <davidcasado1988@yahoo.es>
2023-01-25 13:36:38 +0100

    new Structure

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,22 @@
 function leerTodo(x){
-    x.style.textOverflow = "clip";
+    var etiquetas=document.getElementsByTagName("li");
+    for (var i = 0; i<etiquetas.length; i++) {
+        
+            etiquetas[i].style.textOverflow = "ellipsis";    
+       console.log("ENTRASTE")
+    }
+    x.lastChild.style.textOverflow = "clip";
+   /* x.lastChild.style.textOverflow = "ellipsis";*/
     console.log("les estas dando")
+}
+function cambiarleerTodo(x){
+    var etiquetas=document.getElementsByTagName("li");
+    for (var i = 0; i<etiquetas.length; i++) {
+        
+            etiquetas[i].style.textOverflow = "clip";    
+       console.log("ENTRAST2E")
+    }
+    x.lastChild.style.textOverflow = "ellipsis";
+   /* x.lastChild.style.textOverflow = "ellipsis";*/
+    console.log("les estas2 dando")
 }

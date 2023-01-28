afbda9008cfb17eef47177326845d620690dc5e3
DCasa23 <davidcasado1988@yahoo.es>
2023-01-28 14:45:42 +0100

    Fix Nav

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,28 @@
-function leerTodo(x){
-    var etiquetas=document.getElementsByTagName("div");
-    for (var i = 0; i<etiquetas.length; i++) {
-        
-            etiquetas[i].style.textOverflow = "ellipsis";    
-       console.log("ENTRASTE")
+function leerTodo(x) {
+    var etiquetas = document.getElementsByTagName("div");
+    for (var i = 0; i < etiquetas.length; i++) {
+
+        etiquetas[i].style.textOverflow = "ellipsis";
+        console.log("ENTRASTE")
     }
     x.lastChild.style.textOverflow = "clip";
-   /* x.lastChild.style.textOverflow = "ellipsis";*/
+    /* x.lastChild.style.textOverflow = "ellipsis";*/
     console.log("les estas dando")
 }
-function cambiarleerTodo(x){
-    var etiquetas=document.getElementsByTagName("div");
-    for (var i = 0; i<etiquetas.length; i++) {
-        
-            etiquetas[i].style.textOverflow = "clip";    
-       console.log("ENTRAST2E")
+function cambiarleerTodo(x) {
+    var etiquetas = document.getElementsByTagName("div");
+    for (var i = 0; i < etiquetas.length; i++) {
+
+        etiquetas[i].style.textOverflow = "clip";
+        console.log("ENTRAST2E")
     }
     x.lastChild.style.textOverflow = "ellipsis";
-   /* x.lastChild.style.textOverflow = "ellipsis";*/
+    /* x.lastChild.style.textOverflow = "ellipsis";*/
     console.log("les estas2 dando")
 }
+
+$(".boton").click(function () {
+    $("div").collapse("hide")
+    console.log("les estas2 dando a despelgar")
+    $(this).collapse("show")
+})

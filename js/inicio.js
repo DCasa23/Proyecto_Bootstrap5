62b1f9c3dc69e16d6a905dae988d10e88919ab1c
DCasa23 <davidcasado1988@yahoo.es>
2023-01-28 22:43:17 +0100

    New JQuery

---
+++ js/inicio.js
@@ -0,0 +1,21 @@
+//Observamos el usuario y contraseña guardado, solo se conservan para facilitar la tarea de correción de la tarea.
+console.log("El usuario: "+localStorage.getItem('usuario'));
+console.log("La contraseña: "+localStorage.getItem('password'));
+
+//Si no se introduce un usuario conocido, se observará una ventana emergente para acceder
+
+$("#BotonInicio").click((function (e) {
+    e.preventDefault();
+    if(document.getElementById("usuarioform").value!=localStorage.getItem('usuario') || document.getElementById("passwordform").value!=localStorage.getItem('password')){
+    $("#mensajeCrear").removeClass("d-none");
+    $("#mensajeCrear").addClass("d-block");
+    console.log("El usuario: "+localStorage.getItem('usuario'));
+    }else{
+        window.location.href = 'html/perfil.html';
+    }}))
+
+//Enlace para crear usuario
+
+$("#botonCrear").click(function() {
+    window.location.href = 'html/crear.html';
+})
